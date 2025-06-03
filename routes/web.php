d9356edf8479ac06c0f267bef024649954382222
Nausin Onti <ovlenats@gmail.com>
2025-06-03 17:16:57 +0600

    Onti

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ Route::get('/dashboard', function () {
     return view('pages.dashboard');
 });
 //fardin
+
+
+//onti
+Route::get('/suppliers/home', function () {
+    return view('suppliers_home');
+});
+
+//onti
