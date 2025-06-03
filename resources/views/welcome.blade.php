32878a19cf0641ca083d53b4afc7922a104b49cc
mdsafaet <mdsafet84@gmail.com>
2025-06-03 11:38:19 +0600

    project created

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Kitchen</title>
+     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+      <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+<body>
+
+<h1 class="text-center text-2xl font-bold mt-10">Welcome to My Kitchen</h1>
+    
+</body>
+</html>
